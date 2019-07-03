809bbc02e02d9a5971274c5a2ead6aace30472a9
Mike McGuire <MoonshotMike@gmail.com>
2019-07-03 14:43:08 -0600

    starting variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+/* 
+var, let, const
+
+var is from the original JS
+let and const are standard since 2015
+*/
+
+var name = 'Fred Flintstone';
+console.log(name);
+name = 'Wilma Flintstone';
+console.log(name);
+
+var initializationOnly;
+console.log(initializationOnly);
+
