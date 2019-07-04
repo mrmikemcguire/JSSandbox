20812b474ef712595ca6fd32f1a18975af00a2bb
Mike McGuire <MoonshotMike@gmail.com>
2019-07-04 16:03:46 -0600

    math objects

---
+++ mathObject.js
@@ -0,0 +1,16 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = Math.PI;
+
+//just type Math. to see the built-in functions
+val = Math.
+val = Math.round(2.5);
+val = Math.ceil(2.5);
+val = Math.floor(2.5);
+val = Math.sqrt(64);
+val = Math.max(23, 14, 67, 53);
+val = Math.min(23, 14, 67, 53);
+val = Math.floor(Math.random() * 5 + 1);
+console.log(val);
