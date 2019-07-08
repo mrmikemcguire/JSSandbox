2c92565ab64d413cdf2da70bf4e09845b5ce9311
mrmikemcguire <moonshotmike@gmail.com>
2019-07-08 17:55:28 -0400

    added string objects

---
+++ stringObject.js
@@ -0,0 +1,14 @@
+const firstName = 'Fred';
+const lastName = 'Flintstone';
+let val;
+
+val = firstName + ' ' + lastName;
+val = firstName.concat(' ', lastName);
+
+// val = "That's awesome, isn't it?";
+// val = 'That\'s awesome, isn\'t it?';
+
+// val
+
+console.log(val); 
+
