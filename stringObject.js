d6a84d4a719e10856bd944a802e73be867d5cd92
mrmikemcguire <moonshotmike@gmail.com>
2019-07-08 20:34:14 -0400

    finished string objects

--- stringObject.js
+++ stringObject.js
@@ -1,9 +1,27 @@
 const firstName = 'Fred';
 const lastName = 'Flintstone';
 let val;
+let str;
 
 val = firstName + ' ' + lastName;
 val = firstName.concat(' ', lastName);
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+val = firstName.indexOf('e');
+val = lastName.charAt(3);
+val = lastName.charAt(lastName.length - 1);
+val = lastName.substring(5);
+val = lastName.substring(5, 10);
+val = lastName.slice(0, 4);
+val = lastName.slice(-5);
+
+str = "dog cat moose rat bird";
+str = str.replace('moose', 'mouse');
+val = str.split(' ');
+val = val.includes('mouse');
+
+
+
 
 // val = "That's awesome, isn't it?";
 // val = 'That\'s awesome, isn\'t it?';
