d276e166f7ad6048e8316bfabf08e4f16d9643d4
mrmikemcguire <moonshotmike@gmail.com>
2019-07-08 20:55:52 -0400

    template literals

---
+++ templateLiterals.js
@@ -0,0 +1,23 @@
+const name = 'Fred';
+const age = 40;
+const job = 'construction';
+const city = 'Bedrock';
+
+//without templace literals
+let html;
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+
+// document.body.innerHTML = html;
+
+// with template literals (template strings)
+
+//this is a back tick (left of #1 key)
+html = ` 
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+    </ul>
+`;
+document.body.innerHTML = html;
