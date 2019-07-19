8703d21fc6f7df0522b9aed7731ae79c4fa86a42
mrmikemcguire <moonshotmike@gmail.com>
2019-07-19 12:24:26 -0400

    added Windows object

---
+++ window.js
@@ -0,0 +1,34 @@
+// Window methods & object
+
+//window.alert('Hello World!');
+
+//const input = prompt();
+//alert (input);
+
+// if(confirm('Are you sure?')) 
+//     {
+//     console.log('Yes');
+//     }
+
+let val;
+val = window.outerHeight;
+val = window.outerWidth;
+val = window.innerHeight;
+val = window.innerWidth;
+val = window.location;
+val = window.location.href;
+
+//window.location.href = 'http://google.com';
+
+val = window.history.length;
+window.history.go(-1);
+
+//Navigator object - deals with browser itself
+
+val = window.navigator;
+val = window.navigator.userAgent;
+val = window.navigator.language;
+
+
+
+console.log(val);
